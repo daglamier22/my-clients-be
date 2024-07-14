5886dc0e81452dcee87e5bb710f3caef9ab471a0
Brian McCarthy <daglamier22@gmail.com>
2024-07-14 12:57:37 -0500

    add print statement so i can see the port printed in logs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"my-clients-be-tmp/internal/server"
+	"os"
 )
 
 func main() {
 
 	server := server.NewServer()
 
+	fmt.Printf("Server is running at %v\n\r", os.Getenv("PORT"))
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(fmt.Sprintf("cannot start server: %s", err))
